d9a2ed155afb29c91f0000c8f32c9d43012bc756
Ramya M <ramyam@Ramyas-MacBook-Pro.local>
2023-11-24 13:00:49 -0800

    add all

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          STock stats comes here
         </a>
       </header>
     </div>
